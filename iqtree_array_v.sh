d607739d8a4942ddd7faf32d86aee3c2f27dd1b8
Ollie Jarvis <olliejarvis@MacBook-2.local>
2023-12-04 18:31:58 +0000

    maximum likelihood iqtree array for alignements from the vosseberg dataset

---
+++ iqtree_array_v.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=iqtree_array2
+#SBATCH --partition=compute
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=10
+#SBATCH --time=14-00:00:00
+#SBATCH --mem=100gb
+#SBATCH --array=1-570
+
+threads=10
+
+work_dir=""
+
+module load apps/iqtree/1.6.12
+
+iqtree="/sw/apps/iqtree-1.6.12-Linux/bin/iqtree"
+
+cd $work_dir
+
+$iqtree -s PF.${SLURM_ARRAY_TASK_ID}.fasta -m MFP -bb 1000 -nm 10000 -nt AUTO

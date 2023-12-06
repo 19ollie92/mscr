169ab899794e8e5ff56a6fc019c35abde89c25b3
Ollie Jarvis <olliejarvis@MacBook-2.local>
2023-12-06 18:36:39 +0000

    plot molecular clock analysis output

---
+++ tree_plot.r
@@ -0,0 +1,42 @@
+install.packages("MCMCtreeR")
+
+library(MCMCtreeR)
+
+
+dated.tree <- MCMCtreeR::readMCMCtree("pf_178.tre")
+
+MCMC.tree.plot(dated.tree,
+               cex.tips = 0.3, 
+               time.correction = 100, 
+               scale.res = c("Eon", "Period"), 
+               plot.type = "phylogram", 
+               cex.age = 0.5, 
+               cex.labels = 0.6, 
+               relative.height = 0.1, 
+               col.tree = "grey40", 
+               label.offset = 7, 
+               node.method = "bar",
+               lwd.bar = 1.5,
+               col.age = "lightgreen",
+               ladderize.tree = TRUE,
+               no.margin = FALSE)
+
+MCMC.tree.plot(dated.tree,
+               cex.tips = 0.4, 
+               time.correction = 100, 
+               scale.res = c("Eon", "Period"), 
+               plot.type = "phylogram", 
+               cex.age = 0.4, 
+               cex.labels = 0.6, 
+               relative.height = 0.1, 
+               col.tree = "grey40", 
+               label.offset = 7, 
+               node.method = "none",
+               ladderize.tree = TRUE,
+               no.margin = FALSE)
+
+
+
+
+
+
